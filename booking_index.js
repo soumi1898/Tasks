d6f39b5c6bd378d5557e0f8585e77e36f7852ae4
soumi1898 <153014671+soumi1898@users.noreply.github.com>
2023-12-12 22:19:46 +0530

    Add files via upload

--- booking_index.js
+++ booking_index.js
@@ -1,12 +1,15 @@
 var form=document.getElementById('addForm');
 var itemsList=document.getElementById('items');
 var del=document.querySelectorAll('#deleteBtn')
-
+var countID=0;
 form.addEventListener('submit',addPerson);
+itemsList.addEventListener('click',editPerson);
+
 itemsList.addEventListener('click',removePerson);
 
 function addPerson(e)
 {
+    countID++;
     e.preventDefault();
     let email=document.getElementById('email');
     let name=document.getElementById('username');
@@ -22,11 +25,13 @@ function addPerson(e)
 
     //add class
     li.className='list-group-item';
+    li.id='item'+countID;
 
     var str=localStorage.getItem('name') +"-" +localStorage.getItem('email');
-
+    li.textContent=str;
+   // li.
     //add text node
-    li.appendChild(document.createTextNode(str));
+    //li.appendChild(document.createTextNode(str));
 
         
     //create delete button
@@ -38,7 +43,14 @@ function addPerson(e)
     input.type="button";
     input.className="btn btn-danger btn-sm float-right delete";
     input.id="deleteBtn";
-    input.value="Delete";
+    input.value="DELETE";
+
+    //CREATE EDIT BUTTON
+    var edit=document.createElement('input');
+    edit.type="button";
+    edit.className="btn btn-danger btn-sm float-right edit";
+    edit.id="editBtn";
+    edit.value="EDIT";
 
     //append text node to button
     //deleteBtn.appendChild(document.createTextNode('Delete'));
@@ -51,6 +63,7 @@ function addPerson(e)
     //append button to li
     //li.appendChild(deleteBtn);
     li.appendChild(input);
+    li.appendChild(edit);
 
 
     itemsList.appendChild(li);
@@ -60,13 +73,42 @@ function addPerson(e)
 
 }
 
+function editPerson(e)
+{
+    console.log("in edit");
+    if(e.target.classList.contains('edit'))
+    {
+        console.log("in edit2");
+
+        //get the target li
+        var li=e.target.parentElement;
+        var itemID=e.target.parentElement.id;
+        console.log(document.getElementById(itemID).textContent);
+        console.log("id is "+e.target.parentElement.id);
+
+        //store the text in a string array
+        var s=new Array();
+        s=document.getElementById(itemID).textContent.split('-');
+        console.log(s);
+
+        //populate the input boxes
+        document.getElementById('username').value=s[0];
+        document.getElementById('email').value=s[1];
+
+        //delete the list
+        itemsList.removeChild(li);
+
+    }
+    
+}
+
 function removePerson(e)
 {
     console.log("1");
 
     if(e.target.classList.contains('delete'))
     {
-        console.log(2);
+        //console.log(2);
         if(confirm('Are you sure?'))
         {
             var li=e.target.parentElement;
@@ -74,4 +116,5 @@ function removePerson(e)
         }
 
     }
+
 }
