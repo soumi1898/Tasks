cbf64f147963cbdf436f0b1b0a578fba8439e0f2
soumi1898 <153014671+soumi1898@users.noreply.github.com>
2023-12-12 16:40:47 +0530

    Add files via upload

--- booking_index.js
+++ booking_index.js
@@ -1,6 +1,9 @@
 var form=document.getElementById('addForm');
+var itemsList=document.getElementById('items');
+var del=document.querySelectorAll('#deleteBtn')
 
 form.addEventListener('submit',addPerson);
+itemsList.addEventListener('click',removePerson);
 
 function addPerson(e)
 {
@@ -14,7 +17,7 @@ function addPerson(e)
     console.log(localStorage.getItem('name'));
     console.log(localStorage.getItem('email'));
 
-    var itemsList=document.getElementById('items');
+    //create new person
     var li=document.createElement('li');
 
     //add class
@@ -25,7 +28,50 @@ function addPerson(e)
     //add text node
     li.appendChild(document.createTextNode(str));
 
+        
+    //create delete button
+   // var deleteBtn=document.createElement('button');
+    var input=document.createElement('input');
+
+    //add class name for button
+    //deleteBtn.className="btn btn-danger btn-sm float-right delete";
+    input.type="button";
+    input.className="btn btn-danger btn-sm float-right delete";
+    input.id="deleteBtn";
+    input.value="Delete";
+
+    //append text node to button
+    //deleteBtn.appendChild(document.createTextNode('Delete'));
+    //input.appendChild(document.create)
+
+
+    //deleteBtn.appendChild(document.setAttribute('onclick'));
+
+
+    //append button to li
+    //li.appendChild(deleteBtn);
+    li.appendChild(input);
+
+
     itemsList.appendChild(li);
 
+    console.log(li);
 
+
+}
+
+function removePerson(e)
+{
+    console.log("1");
+
+    if(e.target.classList.contains('delete'))
+    {
+        console.log(2);
+        if(confirm('Are you sure?'))
+        {
+            var li=e.target.parentElement;
+            itemsList.removeChild(li);
+        }
+
+    }
 }
